90b32721d4391457d5a6ce0a0b36c5444f54f42f
asbel4096-web <asbel4096@gmail.com>
2026-04-06 12:39:51 +0200

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -15,7 +15,11 @@ const firebaseConfig = {
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
 export const googleProvider = new GoogleAuthProvider();
-googleProvider.setCustomParameters({ prompt: "select_account" });
+googleProvider.setCustomParameters({
+  prompt: "select_account"
+});
+
 export const db = getFirestore(app);
 export const storage = getStorage(app);
