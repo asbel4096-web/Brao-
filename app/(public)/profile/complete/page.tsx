84434ffee354ebe5b8b65cd5b367eaa9789cf8c6
asbel4096-web <asbel4096@gmail.com>
2026-05-11 00:19:10 +0200

    Add dynamic export to profile complete page

--- app/(public)/profile/complete/page.tsx
+++ app/(public)/profile/complete/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
